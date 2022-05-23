57ea531a153f7f9ede9e2f668d5a33d6f9b8a6ef
Dalu Cheng <lcheng@maplesoft.com>
2022-05-23 12:57:40 -0400

    implemented threading. still has missing data block problem.

--- examples/google_speech/audio_stream_v3.py
+++ examples/google_speech/audio_stream_v3.py
@@ -1,57 +1,51 @@
 #
 # From https://cloud.google.com/speech-to-text/docs/streaming-recognize#perform_streaming_speech_recognition_on_an_audio_stream
 #
-import threading
-import time
+import asyncio
+import queue
 from datetime import datetime
 
-from google.cloud import speech
-
 import pyaudio
-import queue
+from google.cloud import speech
 
 # Audio recording parameters
 RATE = 16000  # sampling rate, the number of frames per second
-# CHUNK = int(RATE / 10)  # 100ms. frames per buffer. I need RATE * 14.5
-CHUNK = 1024 * 8
-CHUNK_LENGTH = CHUNK / RATE  # the length of recording per chunk
+CHUNK = 1024 * 8  # frames per buffer.
+CHUNK_DURATION = CHUNK / RATE  # the duration of a chunk
 
 # Google charges each request rounded up to the nearest increment of 15 seconds
 # We limit each request no more than but close to 15 seconds
 MAX_LENGTH_PER_REQUEST = 15
 
+
 class MicrophoneStream:
     """Opens a recording stream as a generator yielding the audio chunks."""
 
-    def __init__(self, rate, chunk):
+    def __init__(self, rate: int, chunk: int):
         self._rate = rate
         self._chunk = chunk
-
-        # Create a thread-safe buffer of audio data
-        self._buff = queue.Queue()
-        self._closed = True
+        self._buff = queue.Queue()  # a thread-safe buffer of audio data
+        self._closed = True  # stream status flag
 
     def __enter__(self):
         self._audio_interface = pyaudio.PyAudio()
         self._audio_stream = self._audio_interface.open(
             format=pyaudio.paInt16,
-            # The API currently only supports 1-channel (mono) audio
-            # https://goo.gl/z757pE
             channels=1,
             rate=self._rate,
             input=True,
+            output=False,
             frames_per_buffer=self._chunk,
             # Run the audio stream asynchronously to fill the buffer object.
             # This is necessary so that the input device's buffer doesn't
             # overflow while the calling thread makes network requests, etc.
             stream_callback=self._fill_buffer,
         )
-
         self._closed = False
-
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
+        # TODO: parameters, deal with exceptions
         self._audio_stream.stop_stream()
         self._audio_stream.close()
         self._closed = True
@@ -61,33 +55,42 @@ class MicrophoneStream:
         self._audio_interface.terminate()
 
     def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
-        # frame_count equals buffer size.
+        """Call back function to continuously collect data from the audio stream into the buffer.
 
-        # http://people.csail.mit.edu/hubert/pyaudio/docs/#pyaudio.paContinue
-        #print(status_flags)  # output 0
+        see http://people.csail.mit.edu/hubert/pyaudio/docs/#pyaudio.Stream.__init__
+        for the signature of a callback function.
+        frame_count equals the buffer size.
+        During a normal recording, the observed value of status_flag is paContinue.
+        """
+
+        if status_flags in (pyaudio.paInputUnderflow, pyaudio.paInputOverflow):
+            raise Exception  # TODO. add log
 
         duration = time_info.get('current_time', 0) - time_info.get('input_buffer_adc_time', 0)
-        if duration <= 0:
+        if duration <= 0 or duration > CHUNK_DURATION * 3:
             print('error loading time info')  # TODO: log this error
             return None, pyaudio.paContinue  # ignore this data chunk
-        self._buff.put((in_data, duration))
+
+        self._buff.put((in_data.copy(), duration))
         return None, pyaudio.paContinue
 
     def generator(self):
-        data = []
+        """Called to generate audio data read from buffer. """
+        # total length of audio generated in one call to this function.
         length = 0  # in seconds
+
+        data = []
         while not self._closed:
             while True:
                 try:
-                    item = self._buff.get(block=False)
+                    item = self._buff.get(block=True, timeout=CHUNK_DURATION * 1.2)
                     if item is None:
                         return
                     chunk, duration = item
                     data.append(chunk)
                     length += duration
-                    if MAX_LENGTH_PER_REQUEST - length < (CHUNK_LENGTH * 1.3):
-                        print(f"yield data and return, the length is {length}")
+                    if MAX_LENGTH_PER_REQUEST - length < (CHUNK_DURATION * 1.2):
+                        print(f"yield data and return, the length is {length}")  # TODO, log debug
                         # stop reading data from buffer.
                         yield b"".join(data)
                         return
@@ -95,6 +98,7 @@ class MicrophoneStream:
                     break
 
             if len(data) > 0:
+                print('Queue is emtpy. yield data block')
                 yield b"".join(data)
                 data.clear()
 
@@ -102,7 +106,7 @@ class MicrophoneStream:
         return self._closed
 
 
-def listen_print_loop(responses):
+def _listen_print_loop(responses):
     """Iterates through server responses and prints them.
 
     The responses passed is a generator that will block until a response
@@ -136,7 +140,25 @@ def listen_print_loop(responses):
             break
 
 
-def main():
+def _transcribe(client: speech.SpeechClient,
+                config: speech.StreamingRecognitionConfig,
+                requests_queue: queue.Queue):
+    while True:
+        try:
+            requests = requests_queue.get(block=True, timeout=0.05)
+            print(f"start streaming recognize at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
+            responses = client.streaming_recognize(config, requests)
+            requests_queue.task_done()
+
+            # Now, put the transcription responses to use.
+            print(f"start printing responses at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
+            _listen_print_loop(responses)
+        except queue.Empty:
+            print('no more requests.')
+            return
+
+
+async def main():
     # See http://g.co/cloud/speech/docs/languages
     # for a list of supported languages.
     language_code = "zh"  # a BCP-47 language tag
@@ -158,24 +180,25 @@ def main():
     )
 
     with MicrophoneStream(RATE, CHUNK) as stream:
-        print(f"start recording at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
+        requests_queue = queue.Queue(3)  # TODO: put a limit on queue
 
         while not stream.closed():
-            audio_generator = stream.generator()
             requests = (
                 speech.StreamingRecognizeRequest(audio_content=content)
-                for content in audio_generator
+                for content in stream.generator()
             )
 
-            #print(f'number of active threads {threading.active_count()}')
+            try:
+                requests_queue.put_nowait(requests)
+            except queue.Full:
+                print("Google cloud API is too slow. aborting.")
+                break
 
-            print(f"start streaming recognize at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
-            responses = client.streaming_recognize(streaming_config,
-                                                   requests)
-
-            # Now, put the transcription responses to use.
-            print(f"start printing responses at {datetime.utcnow().strftime('%H:%M:%S.%f')}")
-            listen_print_loop(responses)
+            num_threads = requests_queue.qsize()
+            asyncio.gather(
+                *(asyncio.to_thread(_transcribe, client, streaming_config, requests_queue)
+                  for i in num_threads)
+            )
 
 
 if __name__ == "__main__":
